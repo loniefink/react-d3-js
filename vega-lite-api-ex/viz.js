55665e4bf5e6b7eb24b312ac3f8004d97c15b347
Lonie Fink <loniefink@gmail.com>
2025-09-02 09:24:25 -0700

    vega-lite-api-ex: temp v time; line graph; n-zero y

--- vega-lite-api-ex/viz.js
+++ vega-lite-api-ex/viz.js
@@ -2,13 +2,12 @@ import vl from 'vega-lite-api';
 export const viz = vl
   .markLine(
     {
-      size: 5,
-      opacity: 1
+      size: 3,
     }
   )
   .encode(
     vl.x().fieldT('timestamp'), // fieldQ: quantitative (numerical), no zero baseline
-    vl.y().fieldQ('temperature'),
+    vl.y().fieldQ('temperature').scale({ zero: false }),
     //vl.color().fieldQ('weight'),
     //vl.size().fieldQ('mpg').scale({ zero: false }),
     vl.tooltip().fieldN('temperature')
