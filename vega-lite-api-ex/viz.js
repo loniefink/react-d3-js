29c610782e9165de823ac9592f1a16a1cb7eca69
Lonie Fink <loniefink@gmail.com>
2025-09-02 09:19:41 -0700

    vega-lite-api-ex: change of vis

--- vega-lite-api-ex/viz.js
+++ vega-lite-api-ex/viz.js
@@ -2,15 +2,15 @@ import vl from 'vega-lite-api';
 export const viz = vl
   .markCircle(
     {
-      size: 900,
+      size: 500,
       opacity: 0.5
     }
   )
   .encode(
-    vl.x().fieldN('mpg').scale({ zero: false }), // fieldQ: quantitative (numerical), no zero baseline
+    vl.x().fieldQ('mpg').scale({ zero: false }), // fieldQ: quantitative (numerical), no zero baseline
     vl.y().fieldQ('horsepower').scale({ zero: false }),
-    vl.color().fieldQ('weight'),
-    vl.size().fieldQ('mpg').scale({ zero: false }),
+    vl.color().fieldN('origin'),
+    vl.size().fieldQ('horsepower'),
     vl.tooltip().fieldN('name')
   );
 
