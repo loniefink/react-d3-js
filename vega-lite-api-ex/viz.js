ba4dcdc46f967613a2406ff050001afc74df2da3
Lonie Fink <loniefink@gmail.com>
2025-09-02 12:41:44 -0700

    marks-and-channels: x set as origin as channel example

--- vega-lite-api-ex/viz.js
+++ vega-lite-api-ex/viz.js
@@ -2,31 +2,29 @@ import vl from 'vega-lite-api';
 export const viz = vl
   .markCircle(
     {
-      size: 10,
-      //opacity: 0.4
+      size: 200,
+      opacity: 0.7
     }
   )
   .encode(
-    // channels     | Cat | Ord | Qan |
-    // ---------------------------------------
-    // ---------------------------------------
-    // x            |     |     |     |
-    // ---------------------------------------
-    // y            |     |     |     |
-    // ---------------------------------------
-    // size         |     |     |     |
-    // ---------------------------------------
-    // luminosity   |     |     |     |
-    // ---------------------------------------
-    // hue          |     |     |     |
-    // ---------------------------------------
-    // ---------------------------------------
-    // Cat: Categorical
-    // Ord: Ordered
-    // Qan: Quantitative
-    //
-    vl.x().fieldQ('displacement').scale({ zero: false }), // fieldQ: quantitative (numerical), no zero baseline
+
+
+// x and y positions are good to use on any data
+//|------------------------------|
+//| channels   | Cat | Ord | Qan |
+//| :--------: | :-: | :-: | :-: |
+//| x          |  X  |     |     |
+//| y          |     |     |  X  |
+//| size       |     |     |     |
+//| luminosity |     |     |     |
+//| hue        |     |     |     |
+//|------------------------------|
+//• _Cat: Categorical_
+//• _Ord: Ordered_
+//• _Qan: Quantitative_
+//
+    vl.x().fieldO('origin'), // fieldQ: quantitative (numerical), no zero baseline
     vl.y().fieldQ('horsepower').scale({ zero: false }),
-    vl.tooltip().fieldN('name')
+    vl.tooltip().fieldN('name').title('x and y positions are good to use on any data')
   );
 
