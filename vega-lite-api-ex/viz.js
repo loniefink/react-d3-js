cd1f3dc2df43eee2447c1ffb44df5592569e525b
Lonie Fink <loniefink@gmail.com>
2025-09-02 12:56:20 -0700

    marks-and-channels: line chart viz.js file updated with comments

--- vega-lite-api-ex/viz.js
+++ vega-lite-api-ex/viz.js
@@ -6,6 +6,9 @@ export const viz = vl
     }
   )
   .encode(
+    // world population does make sense as an area
+    // areas difference between several categories that are being aggregated into y axis
+    // ex: stacked area chart
 //|------------------------------|
 //| marks      | Pnt | Lin | Ara |
 //| :--------: | :-: | :-: | :-: |
@@ -24,7 +27,7 @@ export const viz = vl
 //• _Qan: Quantitative_
 //
     vl.x().fieldT('timestamp'), // fieldQ: quantitative (numerical), no zero baseline
-    vl.y().fieldQ('temperature').scale({ zero: false }),
+    vl.y().fieldQ('temperature'),
     //vl.color().fieldQ('weight'),
     //vl.size().fieldQ('mpg').scale({ zero: false }),
     vl.tooltip().fieldN('temperature')
