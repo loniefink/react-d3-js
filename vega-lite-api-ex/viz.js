98d627a59ae3ca33d46613d4e376fe88460e7a5a
Lonie Fink <loniefink@gmail.com>
2025-09-02 09:50:31 -0700

    vega-lite-api-ex: country, population, religion; circle pop

--- vega-lite-api-ex/viz.js
+++ vega-lite-api-ex/viz.js
@@ -1,13 +1,13 @@
 import vl from 'vega-lite-api';
 export const viz = vl
-  .markBar(
+  .markCircle(
   )
   .encode(
     // lookup sort for vega-lite
-    vl.x().fieldN('country').sort("-y"), // fieldQ: quantitative (numerical), no zero baseline
-    vl.y().fieldQ('population'),
-    vl.color().fieldN('religion'),
-    //vl.size().fieldQ('mpg').scale({ zero: false }),
+    vl.x().fieldN('country').sort('-y'), // fieldQ: quantitative (numerical), no zero baseline
+    vl.y().fieldN('religion'),
+    vl.size().fieldQ('population'),
+    vl.color().fieldQ('population'),
     vl.tooltip().fieldN('population')
   );
 
