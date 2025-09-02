c133987bc853f81985de9c6396f9b0b030f827e1
Lonie Fink <loniefink@gmail.com>
2025-09-02 09:29:47 -0700

    vega-lite-api-ex: population bar chart

--- vega-lite-api-ex/viz.js
+++ vega-lite-api-ex/viz.js
@@ -1,15 +1,13 @@
 import vl from 'vega-lite-api';
 export const viz = vl
-  .markLine(
-    {
-      size: 3,
-    }
+  .markBar(
   )
   .encode(
-    vl.x().fieldT('timestamp'), // fieldQ: quantitative (numerical), no zero baseline
-    vl.y().fieldQ('temperature').scale({ zero: false }),
+    // lookup sort for vega-lite
+    vl.x().fieldN('country').sort('-y'), // fieldQ: quantitative (numerical), no zero baseline
+    vl.y().fieldQ('population'),
     //vl.color().fieldQ('weight'),
     //vl.size().fieldQ('mpg').scale({ zero: false }),
-    vl.tooltip().fieldN('temperature')
+    vl.tooltip().fieldN('population')
   );
 
