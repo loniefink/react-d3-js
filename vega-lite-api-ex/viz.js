745444d317e6205dc5fd687a3006c9c6e330f9be
Lonie Fink <loniefink@gmail.com>
2025-09-02 09:20:08 -0700

    vega-lite-api-ex: change of vis, hp v mpg, color

--- vega-lite-api-ex/viz.js
+++ vega-lite-api-ex/viz.js
@@ -2,15 +2,14 @@ import vl from 'vega-lite-api';
 export const viz = vl
   .markCircle(
     {
-      size: 500,
-      opacity: 0.5
+      size: 200,
+      opacity: 0.2
     }
   )
   .encode(
     vl.x().fieldQ('mpg').scale({ zero: false }), // fieldQ: quantitative (numerical), no zero baseline
     vl.y().fieldQ('horsepower').scale({ zero: false }),
-    vl.color().fieldN('origin'),
-    vl.size().fieldQ('horsepower'),
+    vl.color().fieldN('origin')
     vl.tooltip().fieldN('name')
   );
 
