0c13ccca2f85669c683576050a68d626dda1b5f7
Lonie Fink <loniefink@gmail.com>
2025-09-03 12:58:50 -0700

    react-viz: radial color chart using d3 arc

--- react-viz/src/App.jsx
+++ react-viz/src/App.jsx
@@ -1,55 +1,62 @@
 import React, { useState, useCallback, useEffect } from 'react'
 import ReactDom from 'react-dom'
+import { arc, csv } from 'd3'
 import './App.css'
+
+// from ../ch1-pt2/src/Mouth.js
 
 // svg canvas config
 const width=960;
 const height=500;
-const radius=20;
 const center_x=(width/2);
 const center_y=(height/2); 
-const initialMousePosition = { x: center_x, y: center_y };
-
-// cvs data config
-const urlCSV = 'https://gist.githubusercontent.com/loniefink/df895e8eaccb46e8fb060cac99e53787/raw/3fbb43a098b316b1adc5ecf8e7071994ce69bb0d/cssNamedColors.csv'
-
 /*
-const fetchText = async (urlCSV) => {
-  const response = await fetch(urlCSV);
-  return await response.text();
-}
+const radius=20;
+const initialMousePosition = { x: center_x, y: center_y };
 */
 
-const message = data => {
-  let message = '';
-  message += Math.round(d3.csvFormat(data).length/1024) + ' kB';
-  message += '\n' + data.length + ' rows';
-  message += '\n' + data.columns.length + ' columns';
-  return message;
-};
+// cvs data config
+const urlCSV = 'https://gist.githubusercontent.com/loniefink/df895e8eaccb46e8fb060cac99e53787/raw/c64eea6fe44fbd3d4de0fc23c3fb9871d5c587a9/cssNamedColors.csv'
+
+const pieArc = arc()
+        .innerRadius(0)
+        .outerRadius(width);
 
 const App = () => {
-  // const[ mousePosition: variable we are updating, 
-  //        setMousePosition: variable we use to set first variable
-  //  useState(initialMousePosition: variable definition for initial state ie. null etc)
-  const [data, setData] = useState(null)
-  /*
-   * useEffect is used here to prevent repeated calls on every loop to host
-   */
-  useEffect(() => {
-    d3.csv(urlCSV).then(data => {
-      setData(data)
-    });
-  }, []); // empty array implies single firing of function
+    // const[ mousePosition: variable we are updating, 
+    //        setMousePosition: variable we use to set first variable
+    //  useState(initialMousePosition: variable definition for initial state ie. null etc)
+    const [data, setData] = useState(null)
+    /*
+     * useEffect is used here to prevent repeated calls on every loop to host
+     */
+    useEffect(() => {
+        d3.csv(urlCSV).then(data => {
+            setData(data)
+        });
+    }, []); // empty array implies single firing of function
 
     if (!data) {
         return <div className="bold">'Loading...'</div>
     }
 
     console.log(data[0]);
-    console.log(data.length);
+    //console.log(data.map(d=>d));
+    //console.log(data.length);
+    //let i=0;   
 
-  return data.map(d => <div style={{ backgroundColor: d['RGB hex value'],  width: width, height: (height/data.length) }}  />);
+    return <svg width={width} height={height}>
+            <g transform={`translate(${center_x}, ${center_y})`}>
+                {data.map((d, i) => (
+                        <path key={i} fill={d['RGB hex value']} d={pieArc({
+                            startAngle: (i / data.length * 2 * Math.PI),
+                            endAngle: ((i+1) / data.length * 2 * Math.PI)
+                        })} />
+                ))}
+                /*
+                */
+            </g>
+        </svg> 
 };
 
 export default App
