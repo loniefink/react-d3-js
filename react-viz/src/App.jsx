f6fe8a4a862e362d3c752d032c752b015035caae
Lonie Fink <loniefink@gmail.com>
2025-09-04 13:53:13 -0700

    react-viz: menu refactor

---
+++ react-viz/src/App.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import ReactDom from 'react-dom'
+import './App.css'
+import { Dropdown } from './Dropdown'
+
+const options = [
+  { value:"dog", label:"Dog" },
+  { value:"cat", label:"Cat" },
+  { value:"hamster", label:"Hamster" },
+  { value:"parrot", label:"Parrot" },
+  { value:"spider", label:"Spider" },
+  { value:"goldfish", label:"Goldfish" }
+]
+
+const initialValue = 'goldfish';
+
+const App = () => {
+  const [selectedValue, setSelectedValue] = useState(initialValue);
+  console.log(selectedValue);
+  return (
+    <div>
+      <label for="pet-select">Choose a pet:</label>
+      <Dropdown options={options} id="pet-select" selectedValue={selectedValue} onSelectedValueChange={setSelectedValue} />
+    </div> 
+  )
+};
+
+export default App
