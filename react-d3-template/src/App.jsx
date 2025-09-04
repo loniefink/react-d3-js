8e22a6b4f3cf5ac8f4baf74fb3b5ea1116629f26
Lonie Fink <loniefink@gmail.com>
2025-09-04 11:17:10 -0700

    react-d3-template: world map projection styled; pre gradicules (lat, long lines)

--- react-d3-template/src/App.jsx
+++ react-d3-template/src/App.jsx
@@ -1,139 +1,59 @@
 import React, { useState, useCallback, useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import { csv, scaleLinear, scaleTime, extent, timeFormat, line, curveNatural } from 'd3'
+import { json, geoPath, geoNaturalEarth1  } from 'd3'
+import { feature, mesh } from 'topojson-client';
 import './App.css'
 
-const csvUrl = 'https://gist.githubusercontent.com/loniefink/2ff9dd5a1e96e492f5ecd18dc7293f00/raw/2dd52c6ceebc5f2dee6c25824a21d0e472902e55/week_temperature_sf.csv';
+// Data
+const jsonUrl = 'https://unpkg.com/world-atlas@2.0.2/countries-50m.json';
 
 const width = 960;
 const height = 500;
-const margin = { top: 20, right: 30, bottom: 65, left: 90 };
-const xAxisLabelOffset = 56;
-const yAxisLabelOffset = 45;
-const xTickOffset = 7;
-const yTickOffset = 10;
-const circleRadius = 3;
-
+const projection = geoNaturalEarth1(),
+    path = geoPath(projection);
 /*
  *
  * App() 
  *
  */
 function App() {
+  // Data
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    const row = (d) => {
-      d.timestamp = new Date(d.timestamp);
-      d.temperature = +d.temperature;
-      return d;
-    };
-    csv(csvUrl, row).then(setData);
+    json(jsonUrl).then(topojsonData => {
+      const {countries, land} = topojsonData.objects;
+      // exclude outer strokes and only include inner strokes
+      setData({
+        land: feature(topojsonData, land),
+        interiors: mesh(topojsonData, countries, (a, b) => a !== b)
+      });
+    });
   }, []);
   
+  // topojson data requires transformation to geojson for easier presentation w/ d3
 
   if (!data) {
     return <pre>Loading...</pre>
   }
 
-  const innerHeight = height - margin.top - margin.bottom;
-  const innerWidth = width - margin.left - margin.right;
-
-  const xValue = d => d.timestamp;
-  const xAxisLabel = 'Time';
-  const yValue = d => d.temperature;
-  const yAxisLabel = 'Temperature';
-
-  const xAxisTickFormat = timeFormat("%a");
-
-  const xScale = scaleTime()
-    .domain(extent(data,xValue))
-    .range([0, innerWidth])
-    .nice();
-
-  const yScale = scaleLinear()
-    .domain(extent(data,yValue))
-    .range([innerHeight, 0])
-    .nice();
-
+  //console.log(feature);
   console.log(data);
-
 
   return (
     <svg width={width} height={height}>
-      <g transform={`translate(${margin.left},${margin.top})`}>
-    // AxisBottom(xScale, innerHeight, xAxisTickFormat);
-      { xScale.ticks().map(tickValue => {
-
-        //console.log(tickValue);
-        return (
-          <g className="tick" key={tickValue} transform={`translate(${xScale(tickValue)},0)`}>
-           <line y2={innerHeight} />
-          <text key={tickValue} style={{ textAnchor: 'middle' }} dy=".71em" y={innerHeight + yTickOffset}>
-            {xAxisTickFormat(tickValue)}
-          </text>
-          </g>
-        )})}
-
-    // yAxisLabel
-      <text
-        className="axis-label"
-        textAnchor="middle"
-        transform={`translate(${-yAxisLabelOffset}, ${innerHeight / 2}) rotate(-90)`}
-      >
-      {yAxisLabel}
-      </text>
-
-    // AxisLeft
-      { yScale.ticks().map(tickValue => {
-
-        //console.log(tickValue);
-        return (
-        <g className="tick" key={tickValue} transform={`translate(0,${yScale(tickValue)})`}>
-         <line x2={innerWidth} />
-          <text
-            key={tickValue}
-            style={{ textAnchor: 'end' }}
-            x={-xTickOffset}
-            dy=".32em"
-          >
-            {tickValue}
-          </text>
-        </g>
-      )})}
-
-    // xAxisLabel
-    <text
-        className="axis-label"
-        textAnchor="middle"
-        x={innerWidth / 2}
-        y={innerHeight + xAxisLabelOffset}
-    >
-      {xAxisLabel}
-    </text>
-
       // Marks ( data, xScale, yScale, xValue, yValue, tooltipFormat ) 
-      // Line
         <g className="marks">
-          <path 
-            d={line().x((d) => xScale(xValue(d))).y((d) => yScale(yValue(d))).curve(curveNatural)(data)}
-          />
-        {
-      // circles
-            data.map(d => (
-              <circle
-                //className="mark"
-                cx={xScale(xValue(d))}
-                cy={yScale(yValue(d))}
-                r={circleRadius}
-              >
-                <title>{xAxisTickFormat(xValue(d))}</title>
-              </circle>
-            ))
-        }
+      // paths
+            <path className="oceans" d={path({type: 'Sphere'})} />
+            {
+              data.land.features.map(feature => (
+              // req projection
+              // line string
+              <path className="land" key={feature.id} d={path(feature)} />
+            ))}
+            <path className="borders" d={path(data.interiors)} />
         </g>
-
-      </g>
     </svg>
   )
 }
