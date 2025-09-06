2e5caa9fa8f5ec2ca9e893c0e24b6ac18a7d3a6a
Lonie Fink <loniefink@gmail.com>
2025-09-05 18:28:43 -0700

    react-d3-template: histogram of missing migrants data set configured

--- react-d3-template/src/App.jsx
+++ react-d3-template/src/App.jsx
@@ -1,79 +1,207 @@
 import React, { useState, useCallback, useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import { max, json, scaleSqrt, geoPath, geoNaturalEarth1, geoGraticule  } from 'd3'
-import { feature, mesh } from 'topojson-client';
-import { useWorldAtlas } from './useWorldAtlas'
-import { useCities } from './useCities'
+import { csv, max, scaleLog, scaleLinear, timeFormat, scaleTime, extent, format, timeMonths, histogram as bin, sum } from 'd3'
 import './App.css'
+import Select from 'react-dropdown-select'
+
+//const csvUrl = 'https://gist.githubusercontent.com/loniefink/e8a217b8acd62b259c380b2a9ed01305/raw/4cd2462336bfc82dc11e5e10e0b6537c7fe5ff9d/iris.csv';
+const csvUrl = 'https://gist.githubusercontent.com/loniefink/7855d4a2d1d1ca889cc83c9b5735236a/raw/71b2b91b6d2652e96ef939f1bded776e48e08f81/missing_migrants%2520-%2520Missing_Migrants_Global_Figures_allData.csv'
 
 const width = 960;
 const height = 500;
-const projection = geoNaturalEarth1(),
-    path = geoPath(projection);
-const graticules = geoGraticule();
-// used in circle fill w/ colors[r-1]
-const colors = [ '#000', '#444', '#666', '#777', '#888' ];
-let maxPopulation = 0;
-let minPopulation = 0;
-const maxRadius = 10;
-function getR(thisPopulation) {
-    let r = 0;
-    if ((maxPopulation - minPopulation) !== 0) {
-      r = 4*((thisPopulation - minPopulation)/(maxPopulation - minPopulation));
-    }
-    return Math.floor(r+1);
-}
+const margin = { top: 20, right: 30, bottom: 65, left: 90 };
+const xAxisLabelOffset = 56;
+const yAxisLabelOffset = 45;
+const xTickOffset = 7;
+const yTickOffset = 10;
+const circleRadius = 2;
+const initialXAttribute = 'date';
+const initialYAttribute = 'dead';
+const attributes = [
+    { value: 'dead', label: 'Total Dead and Missing'},
+    { value: 'date', label: 'Incident Date'},
+    { value: 'coord', label: 'Coordinates'},
+];
+
+
 /*
  *
  * App() 
  *
  */
 function App() {
-  // Data
-  const dataWorld = useWorldAtlas();
-  const dataCities = useCities();
+  const [data, setData] = useState(null);
+  const [xAttribute, setXAttribute] = useState(initialXAttribute);
+  const [yAttribute, setYAttribute] = useState(initialYAttribute);
 
-  // topojson data requires transformation to geojson for easier presentation w/ d3
+  
+  useEffect(() => {
+    const row = (d) => {
+        // date Incident Date	Total Number of Dead and Missing	Coordinates
+      d.dead = +d['Total Number of Dead and Missing'];
+      d.date = new Date(d['Incident Date']);
+      d.coord = +d['Coordinates'];
+      return d;
+    };
+    csv(csvUrl, row).then(setData);
+  }, []);
+  
 
-  if (!dataWorld || !dataCities) {
+  if (!data) {
     return <pre>Loading...</pre>
   }
 
-  maxPopulation = dataCities[0].population;
-  minPopulation = dataCities[(dataCities.length-1)].population;
 
-    const sizeValue = (d) => d.population;
-    const sizeScale = scaleSqrt()
-        .domain([0,max(dataCities, sizeValue)])
-        .range([0, maxRadius]);
+    //console.log(data[0]);
+  const getLabel = (value) => {
+      for (let i=0; i< attributes.length; i++) {
+          if (attributes[i].value === value) {
+              //console.log(attributes[i].label);
+              return attributes[i].label; 
+          }
+      }
+      return attributes[0].label; 
+  }
+
+  const innerHeight = height - margin.top - margin.bottom;
+  const innerWidth = width - margin.left - margin.right;
+
+    // xValue returns petal_length
+  const xValue = d => d[xAttribute];
+  const xAxisLabel = getLabel(xAttribute);
+  const yValue = d => d[yAttribute];
+  const yAxisLabel = getLabel(yAttribute);
+
+  const xAxisTickFormat = timeFormat("%m-%Y")
+
+  const xScale = scaleTime()
+    .domain(extent(data,xValue))
+    .range([0, innerWidth])
+    .nice();
+
+
+  const [start, stop] = xScale.domain();
+
+  const binnedData = bin()
+        .value(xValue)
+        .domain(xScale.domain())
+        .thresholds(timeMonths(start, stop))(data);
+  const aggData = binnedData
+        .map(array => ({
+            totalDeadAndMissing: sum(array,yValue),
+            x0: array.x0,
+            x1: array.x1
+        }))
+  console.log("aggData", aggData);
+  console.log("binnedData", binnedData);
+
+  const yScale = scaleLinear()
+    .domain([0, max(aggData, d=> d.totalDeadAndMissing)])
+    .range([innerHeight, 0])
+
+    console.log(yScale.domain())
+
+
+    const options1 = [];
+    const options2 = [];
+    const options3 = [];
+    const selectedValue = '';
+  //console.log(scaleLinear().ticks());
+
 
 
   return (
-    <svg width={width} height={height}>
-      // Marks ( data, xScale, yScale, xValue, yValue, tooltipFormat ) 
-        <g className="marks">
-      // paths
-            <path className="oceans" d={path({type: 'Sphere'})} />
-            <path className="latAndLongLines" d={path(graticules())} />
-            {
-              dataWorld.land.features.map((feature,i) => (
-              // req projection
-              // line string
-              <path className="land" key={i} d={path(feature)} />
-            ))}
-            <path className="borders" d={path(dataWorld.interiors)} />
-            {
-                  dataCities.map((city,i) => {
-                      let r = getR(city.population);
-                      //console.log("r:",r);
-                      const[x,y] = projection([city.lng, city.lat]);
-                      return (
-                          <circle opacity={0.2} fill={"#c33"} key={i} cx={x} cy={y} r={sizeScale(sizeValue(city))}><title>{city.city + ':' + city.population}</title></circle>
-                      );
-                  })
-            }
-        </g>
-    </svg>
+      <>
+      
+          <div className="select-outer">
+              <div className="select-inner">
+                  <label>X:</label>
+                  <Select
+                      className="select-x"
+                      options={attributes}
+                      placeholder={getLabel(xAttribute)} // default
+                      selectedValue={xAttribute}
+                      onChange={([{value}]) => setXAttribute(value)}
+                  />
+                  <label className="label-y">Y:</label>
+                      
+                  <Select
+                      className="select-y"
+                      options={attributes}
+                      placeholder={getLabel(yAttribute)} // default
+                      selectedValue={yAttribute}
+                      onChange={([{value}]) => setYAttribute(value)}
+                  />
+              </div>
+          </div>
+            <svg width={width} height={height}>
+              <g transform={`translate(${margin.left},${margin.top})`}>
+            // AxisBottom(xScale, innerHeight, xAxisTickFormat);
+              { xScale.ticks().map((tickValue, i) => (
+                  <g className="tick" key={i} transform={`translate(${xScale(tickValue)},0)`}>
+                   <line y2={innerHeight} />
+                  <text key={tickValue} style={{ textAnchor: 'middle' }} dy=".71em" y={innerHeight + yTickOffset}>
+                    {xAxisTickFormat(tickValue)}
+                  </text>
+                  </g>
+                ))}
+
+            // yAxisLabel
+              <text
+                className="axis-label"
+                textAnchor="middle"
+                transform={`translate(${-yAxisLabelOffset}, ${innerHeight / 2}) rotate(-90)`}
+              >
+              {yAxisLabel}
+              </text>
+
+            // AxisLeft
+              { yScale.ticks().map(tickValue => {
+
+                //console.log(tickValue);
+                return (
+                <g className="tick" key={tickValue} transform={`translate(0,${yScale(tickValue)})`}>
+                 <line x2={innerWidth} />
+                  <text
+                    key={tickValue}
+                    style={{ textAnchor: 'end' }}
+                    x={-xTickOffset}
+                    dy=".32em"
+                  >
+                    {tickValue}
+                  </text>
+                </g>
+              )})}
+
+            // xAxisLabel
+            <text
+                className="axis-label"
+                textAnchor="middle"
+                x={innerWidth / 2}
+                y={innerHeight + xAxisLabelOffset}
+            >
+              {xAxisLabel}
+            </text>
+
+            // Marks ( data, xScale, yScale, xValue, yValue, tooltipFormat ) 
+              {
+                /* */
+                aggData.map(d => (
+                  <rect
+                    className="mark"
+                    x={xScale(d.x0)}
+                    y={yScale(d.totalDeadAndMissing)}
+                    width={xScale(d.x1) - xScale(d.x0)}
+                    height={innerHeight - yScale(d.totalDeadAndMissing)}
+                  >
+                    <title>{xAxisTickFormat(d.totalDeadAndMissing)}</title>
+                  </rect>
+                ))
+              }
+
+              </g>
+            </svg>
+      </>
   )
 }
 
